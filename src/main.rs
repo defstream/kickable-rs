32e8b9eb2c83f886438f678f217868027626801b
@DEFSTREAM <defstream@users.noreply.github.com>
2022-12-22 13:05:37 -0800

    slight refactor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod args;
 mod kickable;
 
+const NO: &'static str = "No.";
+const YES: &'static str = "Yes, yes you can.";
+const NO_INPUT: &'static str = "\n👟Kickable let's you know if you can kick it.\n\nUsage: {kickable} <item>\n<item> is the value to check for kick-ability.";
+
 fn main() {
     if std::env::args().len() == 1 {
+        println!("{NO_INPUT}");
         std::process::exit(exitcode::NOINPUT);
     }
     let args = args::parse();
-    let response = kickable::can_i_kick(&args.item);
-    println!("{response}");
+
+    if kickable::validate(&args.item) {
+        println!("{YES}");
+        std::process::exit(exitcode::OK);
+    } else {
+        println!("{NO}");
+        std::process::exit(exitcode::DATAERR);
+    }
 }
