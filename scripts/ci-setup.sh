ee211f37a312cc5de99b7d73e8071920c8ecaf39
@DEFSTREAM <defstream@users.noreply.github.com>
2023-01-23 06:23:34 -0800

    upgrading dependencies

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,6 +1,6 @@
 apt-get update -y
 dpkg --add-architecture arm64 amd64
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip qemu-system binfmt-support qemu-user-static
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip qemu-system binfmt-support qemu-user-static x86_64-w64-mingw32-gcc  gcc-x86-64-linux-gnu mingw-w64
 wget https://github.com/earthly/earthly/releases/download/v0.6.30/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker stop earthly-buildkitd || true
