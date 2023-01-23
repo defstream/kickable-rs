52a1d87466595817d6d69bca439395040c77d023
@DEFSTREAM <defstream@users.noreply.github.com>
2023-01-22 16:22:34 -0800

    additional ci testing-3

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,4 +1,4 @@
 apt-get update -y
-dpkg --add-architecture arm64
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip binfmt-support qemu-user-static docker
+dpkg --add-architecture arm64 amd64
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip binfmt-support qemu-user-static
 wget https://github.com/earthly/earthly/releases/download/v0.6.30/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap
