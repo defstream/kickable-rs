6b81441343e11f0a6cde59ae5f9f5e9be41c252a
@DEFSTREAM <defstream@users.noreply.github.com>
2023-01-22 16:27:51 -0800

    additional ci testing-4

--- scripts/build-setup.sh
+++ scripts/build-setup.sh
@@ -1,6 +1,4 @@
 apt-get update -y
 dpkg --add-architecture arm64
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler zip gcc-x86-64-linux-gnu mingw-w64 qemu-system binfmt-support qemu-user-static docker
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler zip gcc-x86-64-linux-gnu mingw-w64 qemu-system binfmt-support qemu-user-static
 rustup target add x86_64-pc-windows-gnu
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker stop earthly-buildkitd || true
