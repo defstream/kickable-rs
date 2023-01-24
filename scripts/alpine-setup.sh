9b610d854ba40f07adb682813e69aba1d629e01a
@defstream <defstream@users.noreply.github.com>
2023-01-23 17:56:21 -0800

    updating buildspec to target alpine

--- scripts/alpine-setup.sh
+++ scripts/alpine-setup.sh
@@ -1,4 +1,5 @@
-apk add protobuf protoc wrk wget
+apk add protobuf protoc wrk wget docker
 wget https://github.com/earthly/earthly/releases/download/v0.6.30/earthly-linux-amd64 -O /usr/local/bin/earthly
 chmod +x /usr/local/bin/earthly
+service docker start
 /usr/local/bin/earthly bootstrap
--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,6 +1,6 @@
 apt-get update -y
-dpkg --add-architecture arm64 amd64
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip qemu-system binfmt-support qemu-user-static x86_64-w64-mingw32-gcc  gcc-x86-64-linux-gnu mingw-w64
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip qemu-system binfmt-support qemu-user-static x86_64-w64-mingw32-gcc gcc-x86-64-linux-gnu mingw-w64 docker iptables
+wget https://download.docker.com/linux/static/stable/x86_64/docker-20.10.9.tgz && tar -xzvf docker-20.10.9.tgz && cp docker/* /usr/bin/ && dockerd
 wget https://github.com/earthly/earthly/releases/download/v0.6.30/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker stop earthly-buildkitd || true
