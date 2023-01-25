eb258aa1bc5afc1eb4db5c0cb37bc0110be16f74
@defstream <defstream@users.noreply.github.com>
2023-01-24 18:49:54 -0800

    updating host

--- scripts/code-build-setup.sh
+++ scripts/code-build-setup.sh
@@ -1,5 +1,5 @@
 echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" |sudo tee -a /etc/resolv.conf
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip qemu-system binfmt-support qemu-user-static gcc-mingw-w64-x86-64 gcc-x86-64-linux-gnu mingw-w64 iptables
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends zip qemu-system binfmt-support qemu-user-static iptables
 wget https://github.com/earthly/earthly/releases/download/v0.6.30/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap
 cat /etc/docker/daemon.json
 echo '{"iptables":true,"dns": ["8.8.8.8", "8.8.4.4"]}' > /etc/docker/daemon.json
