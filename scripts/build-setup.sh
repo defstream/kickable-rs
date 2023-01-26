10f37b98bd334fb60be2f18af09b46a70c490fbe
@defstream <defstream@users.noreply.github.com>
2023-01-26 11:59:33 -0800

    updating travis setup scrpit

--- scripts/build-setup.sh
+++ scripts/build-setup.sh
@@ -1,4 +1,3 @@
 apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler zip gcc-x86-64-linux-gnu mingw-w64 curl
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 rustup target add x86_64-pc-windows-gnu
--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -1,5 +1,5 @@
 # install rust and protobuf
-apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler curl
+apt-get update -y && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler curl ca-certificates
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+export PATH=$HOME/.cargo/bin:$PATH
 rustup default stable
