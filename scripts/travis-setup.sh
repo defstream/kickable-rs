3f2421b08d73d65f3597b926a85c9c42cfe1639a
@defstream <defstream@users.noreply.github.com>
2023-01-26 12:01:56 -0800

    updating travis setup

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -1,4 +1,4 @@
-# install rust and protobuf
+# install rust and dependencies
 apt-get update -y && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler curl ca-certificates
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 export PATH=$HOME/.cargo/bin:$PATH
