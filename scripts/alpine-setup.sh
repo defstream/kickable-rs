1757ee4b33cd84e650c55f3ccf03479034ebb3d2
@defstream <defstream@users.noreply.github.com>
2023-03-22 01:43:32 -0700

    cleaning up old scripts

--- scripts/alpine-setup.sh
+++
@@ -1,4 +0,0 @@
-apk add protobuf protoc wrk wget docker
-wget https://github.com/earthly/earthly/releases/download/v0.7.2/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap
-service docker start
-/usr/local/bin/earthly bootstrap
--- scripts/release-setup.sh
+++
@@ -1,7 +0,0 @@
-apt-get update -y
-apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt-get update -y
-apt-get install gh -y
