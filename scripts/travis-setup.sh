b295aedee374c5a8cb7ebebc027aecd145122db4
@defstream <defstream@users.noreply.github.com>
2023-03-21 19:04:29 -0700

    removing the specification of tls

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -1,5 +1,5 @@
 # install rust and dependencies
 apt-get update -y && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends protobuf-compiler curl ca-certificates
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+curl -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
 export PATH=$HOME/.cargo/bin:$PATH
 rustup default stable
