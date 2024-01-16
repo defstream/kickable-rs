115e7751307e9056cd3b41aa2f8a1f843462a2c9
@defstream <defstream@users.noreply.github.com>
2024-01-16 12:55:57 -0800

    Update benchmark script to use the PORT variable for the URL

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,11 +1,12 @@
 export PROTOCOL=http
 export ADDR=0.0.0.0
+export PORT=8080
 export URL="$PROTOCOL://$ADDR"
-wrk -c 400 -t 10 -d 30s http://axum.kickable.orb.local:8080/it
-wrk -c 400 -t 10 -d 30s http://gotham.kickable.orb.local:8080/it
-wrk -c 400 -t 10 -d 30s http://graphul.kickable.orb.local:8080/it
-wrk -c 400 -t 10 -d 30s http://poem.kickable.orb.local.local:8080/it
-wrk -c 400 -t 10 -d 30s http://rocket.kickable.orb.local:8080/it
-wrk -c 400 -t 10 -d 30s http://rouille.kickable.orb.local:8080/it
-wrk -c 400 -t 10 -d 30s http://viz.kickable.orb.local:8080/it
-wrk -c 400 -t 10 -d 30s http://warp.kickable.orb.local:8080/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://axum.kickable.orb.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://gotham.kickable.orb.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://graphul.kickable.orb.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://poem.kickable.orb.local.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://rocket.kickable.orb.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://rouille.kickable.orb.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://viz.kickable.orb.local:$PORT/it
+wrk -c 400 -t 10 -d 30s $PROTOCOL://warp.kickable.orb.local:$PORT/it
